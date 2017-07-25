c7bb5013799bc2b6bcd80600dfaa467f8e15766a
cjewell47 <charles.jewell47@gmail.com>
2017-07-25 19:56:16 +0100

    new doc function

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -2,8 +2,19 @@ angular
 .module('ReedsyQueue')
 .controller('MainCtrl', MainCtrl);
 
-MainCtrl.$inject = [];
-function MainCtrl() {
+MainCtrl.$inject = ['Doc'];
+function MainCtrl(Doc) {
   const vm = this;
-
+  vm.create= docCreate;
+  function docCreate(){
+    Doc
+    .save(vm.doc)
+    .$promise
+    .then(() => {
+      console.log('Created');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
 }
